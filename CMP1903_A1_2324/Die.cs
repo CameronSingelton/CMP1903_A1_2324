a7c9e97aa0d0fd5a7d8e7b73dd0a009a7945ddae
CameronSingelton <145766088+CameronSingelton@users.noreply.github.com>
2024-03-04 09:06:45 +0000

    getter and setters

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -6,23 +6,25 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Die
+    internal class Die : Random
     {
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
-
+        private int _dice_value = 6;
         //Property
-        int _dice_value = 0;
-
+        public int dice_value 
+        {
+            get { return _dice_value; }
+            set { _dice_value = value; }
+        }
         //Method
-        public int Roll() 
-          {
-          Random random = new Random();
-          _dice_value = random.Next(1, 7);
-          return _dice_value; 
+        public int Roll()
+        {
+            Random random = new Random();
+            _dice_value = random.Next(1,7);
+            return _dice_value;
           }
-
     }
 }
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -20,15 +20,20 @@ namespace CMP1903_A1_2324
         //Methods
         public void Roll_dice() 
           {
-            int Dice_Value_1=Dice_1.Roll();
-            int Dice_Value_2=Dice_2.Roll();
-            int Dice_Value_3=Dice_3.Roll();
-            statistics(Dice_Value_1, Dice_Value_2, Dice_Value_3);
-          }
+            Random random = new Random();
+            //for (int i = 0; i < 100; i++)
+            {
+                //Console.WriteLine(random.Next(1, 7));
+                Dice_1.Roll();
+                Dice_2.Roll();
+                Dice_3.Roll();
+                
+                statistics(Dice_1.dice_value, Dice_2.dice_value, Dice_3.dice_value); }
+            }
         public void statistics (int Dice_Value_1, int Dice_Value_2, int Dice_Value_3) 
         {
             int Total_Value = Dice_Value_1 + Dice_Value_2 + Dice_Value_3;
-            Console.WriteLine(Total_Value);
+            Console.WriteLine($"1={Dice_Value_1}\n2={Dice_Value_2}\n3={Dice_Value_3}\ntotal={Total_Value}");
         }
 
     }
