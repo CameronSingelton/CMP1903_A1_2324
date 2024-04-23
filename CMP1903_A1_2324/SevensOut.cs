9e1e3431f209ddf51a0a8e735db0c52322b684c0
CameronSingelton <145766088+CameronSingelton@users.noreply.github.com>
2024-04-23 14:17:19 +0100

    completed 7out

--- CMP1903_A1_2324/SevensOut.cs
+++ CMP1903_A1_2324/SevensOut.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class SevensOut:Die,Igame
+    internal class SevensOut:Igame
     {
         public int Game()
         {
-
-            return 0;
+            int totalvalue = 0;
+            Die Die1 = new Die();
+            Die Die2 = new Die();
+            while (Die1.Dice_value + Die2.Dice_value != 7)
+            {
+                Die1.Roll();
+                Die2.Roll();
+                if ((Die1.Dice_value + Die2.Dice_value) == 7)
+                {
+                    break;
+                }
+                else if (Die1.Dice_value == Die2.Dice_value)
+                {
+                    totalvalue += (Die1.Dice_value+Die2.Dice_value)*2;
+                    continue;
+                }
+                else
+                {
+                    totalvalue += Die1.Dice_value + Die2.Dice_value;
+                    continue; 
+                }
+            }
+            return totalvalue;
         }
     }
 }
--- CMP1903_A1_2324/ThreeOrMore.cs
+++ CMP1903_A1_2324/ThreeOrMore.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class ThreeOrMore:Die
+    internal class ThreeOrMore:Die,Igame
     {
+        public int Game()
+        {
+            List<int> list = new List<int>();
+            //Die die = new Die();
+           for (int i = 0; i >= 5; i++)
+            {
+                this.Roll();
+                list.Add(this.Dice_value);
+            }
+            return 0;
+        }
 
     }
 }
